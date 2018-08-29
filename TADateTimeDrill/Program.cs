8741a4272679df517aba103b2528e1b9b7b234e1
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-29 13:24:07 -0400

    Datetime test

--- TADateTimeDrill/Program.cs
+++ TADateTimeDrill/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO
 
 namespace TADateTimeDrill
 {
@@ -10,6 +11,15 @@ namespace TADateTimeDrill
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Give me a number.");
+            string num = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Keenan\Desktop\IOtest.txt", num);
+            string userInput = File.ReadAllText(@"C:\Users\Keenan\Desktop\IOtest.txt");
+            Console.WriteLine(userInput);
+
+            Console.ReadLine();
         }
     }
 }
